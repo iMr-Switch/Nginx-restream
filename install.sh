fe6da419bf1dc248ce513279b53e80510b8a12a7
antonio-colmakers <antonio@colmakers.com>
2018-03-08 22:17:26 -0500

    Change mode to install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ wget https://nginx.org/download/nginx-1.12.2.tar.gz
 tar xzf nginx-1.12.2.tar.gz
 sudo apt-get install -y gcc
 sudo apt-get install -y libpcre3 libpcre3-dev build-essential libssl-dev
-cd nginx-1.12.2
+cd nginx-1.12.2/
 ./configure --add-module=/root/Nginx-restream/nginx-rtmp-module
 make
 make install
