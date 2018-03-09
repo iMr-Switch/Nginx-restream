dfa6fee3b99577137dcdeb1b8c763751ca6503da
antonio-colmakers <antonio@colmakers.com>
2018-03-08 21:50:54 -0500

    Add rmtp confg

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get upgrade
+wget https://nginx.org/download/nginx-1.12.2.tar.gz
+tar xzf nginx-1.12.2.tar.gz
+sudo apt-get install -y gcc
+sudo apt-get install -y libpcre3 libpcre3-dev build-essential libssl-dev
+cd nginx-1.12.2
+./configure --add-module=/root/nginx-rtmp-module
+make
+make install
+/usr/local/nginx/sbin/nginx -s stop
+cp -r nginx.conf /usr/local/nginx/conf/
