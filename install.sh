036cca3414238a79c90858e4bb1ec776a43079be
Antonio Oviedo <37199440+iMrSwitch@users.noreply.github.com>
2018-07-31 12:35:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ wget https://nginx.org/download/nginx-1.12.2.tar.gz
 tar xzf nginx-1.12.2.tar.gz
 sudo apt-get install -y gcc
 sudo apt-get install -y libpcre3 libpcre3-dev build-essential libssl-dev
+route="$PWD/nginx-rtmp-module"
 cd nginx-1.12.2/
-./configure --add-module=/root/Nginx-restream/nginx-rtmp-module
+./configure --add-module=$route
 make
 make install
 /usr/local/nginx/sbin/nginx -s stop
